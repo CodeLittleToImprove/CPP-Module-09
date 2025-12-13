68880a6e9f44b095b589fac137e91fe185823bec
Thang Bui Quoc <tbuiloves42@gmail.com>
2025-12-13 21:32:15 +0100

    added csv read from file with validation to ex 00

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -8,18 +8,18 @@ BitcoinExchange::BitcoinExchange()
 }
 
 // Copy constructor
-BitcoinExchange::BitcoinExchange(const BitcoinExchange &other)
-	:_atribute(other._atribute)
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other)
+	:_rates(other._rates)
 {
 	// std::cout << "Copy constructor called" << std::endl;
 }
 
 // Copy Assignment operator overload
-BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &other)
+BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange& other)
 {
 	// std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
-		this->member = other.member();
+		this->_rates = other._rates;
 	return (*this);
 }
 
@@ -29,4 +29,90 @@ BitcoinExchange::~BitcoinExchange(void)
 	// std::cout << "Destructor called" << std::endl;
 }
 
+bool	isValidDate(const std::string& date_str)
+{
+	//YYYY-MM-DD
+	//Check length (4 + 1 + 2 + 1 + 2 = 10)
+	if (date_str.length() != 10)
+		return (false);
 
+	//Check hyphen placement
+	if (date_str[4] != '-' || date_str[7] != '-')
+		return (false);
+	int year, month, day;
+	char hyph1, hyph2;
+
+	std::istringstream iss(date_str);
+	if (!(iss >> year >> hyph1 >> month >> hyph2 >> day))
+		return (false);
+
+	std::string junk;
+	if (iss >> junk)
+		return false;
+
+	int daysInMonth[] =
+	{
+		31, 28, 31, 30, 31, 30,
+		31, 31, 30, 31, 30, 31
+	};
+
+	bool isLeap = (year % 4 == 0 && year % 100 != 0) ||
+		(year % 400 == 0);
+
+	if (isLeap && month == 2)
+		daysInMonth[1] = 29;
+
+	if (day < 1 || day > daysInMonth[month - 1])
+		return false;
+	return (true);
+}
+
+void	BitcoinExchange::parseDatabaseLine(const std::string& line)
+{
+	std::string::size_type commaPos = line.find(',');
+	if (commaPos == std::string::npos)
+		throw InvalidLineError();
+
+	std::string date = line.substr(0, commaPos);
+	std::string price = line.substr(commaPos + 1);
+
+	if (!isValidDate(date))
+		throw InvalidDateError();
+
+	double price_rate;
+	std::istringstream iss2(price);
+	if (!(iss2 >> price_rate))
+		throw InvalidPriceError();
+
+	std::string junk;
+	if (iss2 >> junk)
+		throw InvalidPriceError();
+
+	if (price_rate < 0)
+		throw InvalidPriceError();
+	_rates[date] = price_rate;
+	// debug
+	// std::cout << "\nStream Status Check:" << std::endl;
+	// std::cout << "  Good: " << (iss.good() ? "True" : "False") << std::endl;
+	// std::cout << "  Fail: " << (iss.fail() ? "True" : "False") << std::endl;
+	// std::cout << "  EOF:  " << (iss.eof() ? "True" : "False") << std::endl;
+}
+
+void	BitcoinExchange::loadDatabase()
+{
+	std::ifstream file(DATA_BASE_FILE_NAME);
+	if (!file.is_open())
+		throw OpenFileError();
+
+	std::string line;
+	if (!std::getline(file, line)) //Check if file can be read at at all
+		throw ColumnInvalidError();
+	line.erase(line.find_last_not_of(" \t\r\n") + 1);
+	if (line != "date,exchange_rate")
+		throw ColumnInvalidError();
+
+	while (std::getline(file, line))
+	{
+		parseDatabaseLine(line);
+	}
+}
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -1,14 +1,80 @@
 #pragma once
 
 # include <iostream>
+# include <fstream>
+# include <sstream>
+# include <map>
+# include <exception>
+# include <string>
 
+#define DATA_BASE_FILE_NAME "data.csv"
 class BitcoinExchange
 {
 	private:
-
+		std::map<std::string, double> _rates;
 	public:
 		BitcoinExchange();
 		BitcoinExchange(const BitcoinExchange &other);
 		BitcoinExchange& operator=(const BitcoinExchange &other);
 		~BitcoinExchange();
+
+		void	loadDatabase();
+		void	processInputFile(const std::string& filename);
+
+	private:
+		void	parseDatabaseLine(const std::string& line);
+		void	processLine(const std::string& line);
+
+		// bool	isValidDate(const std::string &date_str);
+		// bool	isLeapYear(int year) const;
+		// bool	isValidValue(const std::string& value) const;
+		// bool	isNumber(const std::string& value) const;
+
+		std::string findClosestLowerData(const std::string& date) const;
+		double	getExchangeRate(const std::string& date) const;
+
+		void	printError(const std::string& message) const;
+
+	class OpenFileError : public std::exception
+	{
+	public:
+		virtual const char* what() const throw()
+		{
+			return ("File could not be opened");
+		}
+	};
+
+	class ColumnInvalidError : public std::exception
+	{
+	public:
+		virtual const char* what() const throw()
+		{
+			return ("Invalid column error. Should be 'date,exchange_rate'.");
+		}
+	};
+
+	class InvalidLineError : public std::exception
+	{
+	public:
+		virtual const char* what() const throw()
+		{
+			return ("Invalid line error. A token is missing");
+		}
+	};
+	class InvalidDateError : public std::exception
+	{
+	public:
+		virtual const char* what() const throw()
+		{
+			return ("Invalid Date error. Something is wrong with the date");
+		}
+	};
+	class InvalidPriceError : public std::exception
+	{
+	public:
+		virtual const char* what() const throw()
+		{
+			return ("Invalid line error. Something is wrong with the price");
+		}
+	};
 };
